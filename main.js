b60ac24c13923e6abe3868537e9929d74c8f9939
Natasha <kalachevan615@gmail.com>
2019-10-15 18:08:11 +0300

    close burger menu

--- main.js
+++ main.js
@@ -6,5 +6,14 @@
   navBtn.addEventListener("click", function() {
     navBtn.classList.toggle("active");
     navMenu.classList.toggle("active");
+    closeMenu();
   });
+  function closeMenu() {
+    document.body.addEventListener("click", function(e) {
+      if (!e.target.closest(".navbar")) {
+        navMenu.classList.remove("active");
+        navBtn.classList.remove("active");
+      }
+    });
+  }
 })(); //main
